ee271eae0e8559ec94e5cd79206d8015dd2323ee
Anicodebekeh <102191636+Anicodebekeh@users.noreply.github.com>
2023-09-14 22:12:48 +0100

    deleteimages using filename from cloudinary and the database integrated

--- controller/blog.js
+++ controller/blog.js
@@ -1,6 +1,7 @@
 const {Blog} = require('../model/blogSchema');
 const wrapAsync = require('../utils/wrapAsync')
-const AppError = require('../utils/appError')
+const AppError = require('../utils/appError');
+const { cloudinary } = require('../cloudinary');
 
 // index controller
 module.exports.index=async(req, res)=>{
@@ -55,6 +56,7 @@ module.exports.delete=wrapAsync(async (req, res)=>{
 // put controller
 module.exports.put = wrapAsync(async (req, res)=>{
     const {id} = req.params
+    // console.log(req.body)
     const foundBlog = await Blog.findById(id)
     if(!foundBlog._id.equals(id)){
         throw new AppError('invalid Blog', 404)
@@ -63,6 +65,13 @@ module.exports.put = wrapAsync(async (req, res)=>{
     const img = req.files.map(i => ({url: i.path, filename: i.filename}))
     blog.images.push(...img)
     await blog.save()
+    if (req.body.deleteImages){
+        for (let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename)
+        }
+        await blog.updateOne ({$pull: {images: {filename: {$in: req.body.deleteImages}}}})
+        console.log(blog)
+    }
     req.flash('success', 'Edited successfully')
     return res.redirect(`/blog/${blog._id}`)
 });
